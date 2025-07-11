9509e5f293073209df70e23f7a81b90455d801bf
Mikooooooooooooooo <miraskazymurat@gmail.com>
2025-07-11 19:50:01 +0500

    Finalized login UI + backend connection

--- frontend/src/components/Input.vue
+++ frontend/src/components/Input.vue
@@ -1,13 +1,39 @@
 <template>
     <div class="input">
-        <input :placeholder="text" v-model="value"/>
+        <input :placeholder="text" :disabled="disabled" @keyup.enter="returnCode" v-model="value"/>
     </div>
 </template>
 
+<style scoped>
+input{
+    outline: none ; 
+    width: calc(100% - 32px) ; 
+    border: 1px solid var(--border-color) ; 
+    background: var(--bg-color) ; 
+    padding: 15px ; 
+    color: var(--option-color) ; 
+    font-size: 16px ; 
+    border-radius: 10px ; 
+    transition: all ease .2s ; 
+}
+input:hover{
+    color: var(--primary-color) ; 
+    border-color: var(--primary-color) ;  
+}
+</style>
+
 <script setup>
+    import {ref} from 'vue'
     const props = defineProps({
-        text: String
+        text: String , 
+        disabled: Boolean
     })  
 
-    const value = ""
+    const value = ref("")
+
+    const emit = defineEmits(['getValue'])
+
+    function returnCode() {
+        emit('getValue' , value.value)
+    } 
 </script>
--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -3,22 +3,23 @@
         <div class="container">
             <Logo class="logo" type="m"/>
             <div class="edit">
-                <BaseText type="font-main"> {{ phone }} </BaseText>
+                <BaseText type="font-main"> {{ formatPhone(phone) }}</BaseText>
                 <div class="icon_container">
                    <img class="icon" src="@/assets/edit.png" />
                 </div>
             </div>
             <BaseText type="subtitle">Enter your telegram login code</BaseText>
-            <div class="input">
-            </div>
+            <Input @getValue="getPass" text="Code" class="input" :disabled="isDisabled"/>
+            <div class="error" v-if="error">{{error}}</div>
         </div>
     </div>
 </template>
 
 <style scoped>
 .container{
+    text-align: center ; 
     padding-top: 10vh ; 
-    width: 500px ; 
+    width: 350px ; 
     margin: 0 auto ; 
 }
 .logo{
@@ -37,13 +38,62 @@ img{
 .icon_container{
     height: 35.2px ; 
 }
+.input{
+    margin-top: 5vh ; 
+    width: 100% ; 
+}
+.error {
+  color: #8f3838; 
+  font-size: 14px;
+  margin-top: 8px;
+}
 </style>
 
 <script setup>
+    import {ref} from 'vue'
     import BaseText from '@/components/BaseText.vue'
     import Logo from '@/components/TelegramLogo.vue' 
+    import Input from '@/components/Input.vue' 
     import {useRoute} from 'vue-router'
+    import api from '@/api'
 
     const route = useRoute() 
     const phone = route.query.phone 
+    const error = ref(null)
+    const isDisabled = ref(false)
+
+    
+    function formatPhone(raw) {
+      const digits = raw.replace(/\D/g, '')
+      if (!digits.startsWith('7')) return raw
+    
+      const parts = [
+        '+7',
+        digits.slice(1, 4),
+        digits.slice(4, 7),
+        digits.slice(7, 9),
+        digits.slice(9)
+      ].filter(Boolean)
+    
+      return parts.join(' ')
+    }
+
+    function getPass(code){
+        isDisabled.value = true 
+        api.post('/login' , {phone: phone , password: code})
+            .then(res => {
+                console.log("Login success")
+            })
+            .catch(err => {
+                error.value = "Wrong code. Try again"
+                console.error("Wrong password")
+                setTimeout(() => {
+                    error.value = null
+                }, 3000)
+            })
+            .finally(() => {
+                isDisabled.value = false
+            })
+    }
+
 </script>
